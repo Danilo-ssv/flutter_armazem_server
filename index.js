34f0808bcb73183cbf7b0068aaa76ea819040bc9
Danilo-ssv <danilosouza20033002@gmail.com>
2023-11-09 08:58:07 -0300

    .env config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./src/main');
 const app = express();
-const PORT = 8080;
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.json())
@@ -14,13 +14,13 @@ app.use('/', (req, res, next)=>{
 	next()
 }, router)
 
-mongoose.connect('mongodb+srv://USER_DANILO:maceio123@cluster0.zzmnspx.mongodb.net/flutter_armazem?retryWrites=true&w=majority', {
+mongoose.connect(process.env.DATABASE_CONNECTION, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 })
 .then(async () => console.log('Successful connection!'))
 .catch(err => console.log('Connection error!', err))
 
-app.listen(PORT || 8080, ()=>{
-	console.log(`Running at port: ${PORT}`)
+app.listen(process.env.PORT , ()=>{
+	console.log(`Running at port: ${process.env.PORT}`)
 })
