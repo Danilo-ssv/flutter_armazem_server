75b94b74c247f2cfe4656c70996b7ddc818ec9e3
Danilo-ssv <danilosouza20033002@gmail.com>
2023-11-14 10:56:41 -0300

    'mudanças'

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ const bodyParser = require('body-parser');
 const productsSchema = new mongoose.Schema({
     id: {type: Number, required: true},
     productName: {type: String, required: true},
+    productCategory: {type: String, required: true},
     productBrand: {type: String, required: true},
     productStock: {type: Number, required: true},
+    productPurchase: {type: Number, required: true},
+    productSale: {type: Number, required: true},
+    profitMargin: {type: Number, required: true},
 })
 const clientsSchema = new mongoose.Schema({
     id: {type: Number, required: true},
@@ -14,6 +18,8 @@ const clientsSchema = new mongoose.Schema({
     clientPhone: {type: String, required: true},
     clientCpf: {type: String, required: true},
     clientDebt: {type: Number, required: true},
+    clientLimit: {type: Number, required: true},
+    paymentDate: {type: String, required: true},
 })
 const productModel = mongoose.model('products_collections', productsSchema)
 const clientModel = mongoose.model('clients_collections', clientsSchema)
@@ -41,26 +47,33 @@ router.post('/export', async (req, res) => {
     await productModel.deleteMany()
     await clientModel.deleteMany()
 
-    productsList.map(product => {
+    productsList.map(async (product) => {
         let newProduct = new productModel({
             id: product.id,
             productName: product.productName,
+            productCategory: product.productCategory,
             productBrand: product.productBrand,
             productStock: product.productStock,
+            productPurchase: product.productPurchase,
+            productSale: product.productSale,
+            profitMargin: product.profitMargin,
         })
-        newProduct.save().then(()=>{}).catch(err => res.send({success: 'failed'}))
+        const sucess = await newProduct.save()
+        // return res.send({success: 'failed'})
     })
-    clientsList.map(client => {
+    clientsList.map(async (client) => {
         let newClient = new clientModel({
             id: client.id,
             clientName: client.clientName,
             clientPhone: client.clientPhone,
             clientCpf: client.clientCpf,
             clientDebt: client.clientDebt,
+            clientLimit: client.clientLimit,
+            paymentDate: client.paymentDate == '' ? '-' : client.paymentDate,
         })
-        newClient.save().then(()=>{}).catch(err => res.send({success: 'failed'}))
+        const sucess = await newClient.save()
     })
-    return res.send({success: 'success'})
+    res.send({success: 'success'})
 })
 
 module.exports = router
